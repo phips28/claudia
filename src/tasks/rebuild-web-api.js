5e8e90184af3decc57911cfeb30345c94a6a4326
phips28 <phips28@gmx.at>
2016-10-24 00:10:43 -0700

    disabled updateStage

--- src/tasks/rebuild-web-api.js
+++ src/tasks/rebuild-web-api.js
@@ -189,32 +189,33 @@ module.exports = function rebuildWebApi(functionName, functionVersion, restApiId
 						'application/json': ''
 					},
 					responseParameters: responseParams
-				}).then(function () {
-					console.log('------');
-					console.log({
-						restApiId: restApiId,
-						stageName: functionVersion,
-						patchOperations: [
-							{
-								op: 'replace',
-								path: '/' + resourceId + '/OPTIONS/caching/enabled',
-								value: 'false'
-							}
-						]
-					});
-					console.log('------');
-					return apiGateway.updateStageAsync({
-						restApiId: restApiId,
-						stageName: functionVersion,
-						patchOperations: [
-							{
-								op: 'replace',
-								path: '/' + resourceId + '/OPTIONS/caching/enabled',
-								value: 'false'
-							}
-						]
-					});
-			  });
+				})
+					// .then(function () {
+					// console.log('------');
+					// console.log({
+					// 	restApiId: restApiId,
+					// 	stageName: functionVersion,
+					// 	patchOperations: [
+					// 		{
+					// 			op: 'replace',
+					// 			path: '/' + resourceId + '/OPTIONS/caching/enabled',
+					// 			value: 'false'
+					// 		}
+					// 	]
+					// });
+					// console.log('------');
+					// return apiGateway.updateStageAsync({
+					// 	restApiId: restApiId,
+					// 	stageName: functionVersion,
+					// 	patchOperations: [
+					// 		{
+					// 			op: 'replace',
+					// 			path: '/' + resourceId + '/OPTIONS/caching/enabled',
+					// 			value: 'false'
+					// 		}
+					// 	]
+					// });
+			  // });
 			});
 		},
 		findResourceByPath = function (path) {
